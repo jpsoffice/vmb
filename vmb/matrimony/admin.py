3baf6814776aa3d6b516f466cd687bab3902142c
Ratnadeep Debnath <rtnpro@gmail.com>
2020-04-11 21:21:36 +0530

    Fix matrimony models.
    - Fix nomenclature
    - Added timestamp fields

--- vmb/matrimony/admin.py
+++ vmb/matrimony/admin.py
@@ -6,7 +6,7 @@ from admin_numeric_filter.admin import (
     SliderNumericFilter,
 )
 from django.contrib import admin
-from .models import MatrimonyProfile, Guru, Language, Degree, Occupation
+from .models import MatrimonyProfile, Guru, Language, Qualification, Occupation
 
 
 # Register your models here.
@@ -24,7 +24,10 @@ class MatrimonyProfileInline(admin.TabularInline):
 class MatrimonyProfileAdmin(NumericFilterModelAdmin):
     fieldsets = [
         (None, {"fields": ["name", ("gender", "marital_status")]}),
-        ("SPIRITUAL QUOTIENT", {"fields": ["rounds_chanting", ("s_status", "guru")]}),
+        (
+            "SPIRITUAL QUOTIENT",
+            {"fields": ["rounds_chanting", ("spiiritual_status", "guru")]},
+        ),
         (
             "BIRTH DETAILS",
             {
@@ -40,8 +43,11 @@ class MatrimonyProfileAdmin(NumericFilterModelAdmin):
             {"fields": ["current_country", ("current_state", "current_city")]},
         ),
         ("PHYSICAL APPEARANCE", {"fields": [("height", "complexion")]}),
-        ("QUALIFICATON", {"fields": ["degree", ("occupation", "monthly_income")]}),
-        ("CONTACT INFORMATION", {"fields": [("phone", "email_id")]}),
+        (
+            "QUALIFICATON",
+            {"fields": ["qualification", ("occupation", "annual_income")]},
+        ),
+        ("CONTACT INFORMATION", {"fields": [("phone", "email")]}),
     ]
     inlines = [MatrimonyProfileInline]
     list_display = (
@@ -51,9 +57,9 @@ class MatrimonyProfileAdmin(NumericFilterModelAdmin):
         "current_country",
         "current_city",
         "occupation",
-        "monthly_income",
+        "annual_income",
         "phone",
-        "email_id",
+        "email",
     )
     list_filter = (
         "current_state",
