dc4fe55601461c41d5077321b948bccd244d9b5b
Ratnadeep Debnath <rtnpro@gmail.com>
2020-04-12 17:57:41 +0530

    Allow blank entries for model timestamp fields.

--- vmb/matrimony/models/base.py
+++ vmb/matrimony/models/base.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
 
     class Meta:
         abstract = True
