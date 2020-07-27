9879df316728d8bb2908e55273c2509fc24e3c06
Ratnadeep Debnath <rtnpro@gmail.com>
2020-07-27 13:02:00 +0530

    Fix language str representation.

--- vmb/matrimony/models/relations.py
+++ vmb/matrimony/models/relations.py
@@ -60,7 +60,7 @@ class Language(BaseModel):
         ordering = ["name"]
 
     def __str__(self):
-        return f"{self.name} {self.code}"
+        return f"{self.name} ({self.code})"
 
 
 class Education(BaseModel):
