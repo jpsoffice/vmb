89331a8d66a2abed3111bc4bdce1837f9355d0f9
Ratnadeep Debnath <rtnpro@gmail.com>
2020-08-07 20:46:31 +0530

    Update production domain.

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["rtnpro.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["vaisnavamarriage.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -121,7 +121,7 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="vmb <noreply@rtnpro.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="vmb <noreply@vaisnavamarriage.com>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
