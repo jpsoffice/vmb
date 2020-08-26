d6dac53d094f266efd36b8fe35b93532f1c10af6
bhawanabadlani <bhawanabadlani@gmail.com>
2020-08-26 19:51:20 +0530

    Included profile_id in read_only set

--- vmb/matrimony/admin.py
+++ vmb/matrimony/admin.py
@@ -410,6 +410,7 @@ class BaseMatrimonyProfileAdmin(DjangoQLSearchMixin, NumericFilterModelAdmin):
     ]
 
     readonly_fields = [
+        "profile_id",
         "age",
         "primary_image",
         "annual_income_in_base_currency",
