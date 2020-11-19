23d8678ff81ad8d08cc6c017c44d326b4ec31a8a
bhawanabadlani <bhawanabadlani@gmail.com>
2020-11-19 13:27:09 +0530

    Remove redundancy in matches by making male and female unique_together.

--- vmb/matrimony/models/profiles.py
+++ vmb/matrimony/models/profiles.py
@@ -942,6 +942,8 @@ class Match(BaseModel):
             models.Index(fields=["female"]),
         ]
 
+        unique_together = [["male", "female"]]
+
         verbose_name = "Match"
         verbose_name_plural = "Matches"
 
