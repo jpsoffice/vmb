a95f346986f2169e369e97d38746234a84d4f69b
bhawanabadlani <bhawanabadlani@gmail.com>
2020-12-31 17:31:40 +0530

    Delete user profile when MP profile is deleted.

--- vmb/matrimony/models/profiles.py
+++ vmb/matrimony/models/profiles.py
@@ -4,6 +4,7 @@ from hashlib import md5
 from django.conf import settings
 from django.db import models
 from django.db.models import Q
+from django.db.models.signals import post_delete
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.template import loader, Context
@@ -1090,3 +1091,12 @@ class MatrimonyProfileStats(BaseModel):
 
     class Meta:
         db_table = "matrimony_profile_stats"
+
+
+def post_delete_user(sender, instance, **kwargs):
+    if instance.user:
+        instance.user.delete()
+
+
+post_delete.connect(post_delete_user, sender=Male)
+post_delete.connect(post_delete_user, sender=Female)
