d0cdaf06db643e681d886516e753fb26fdc540ac
Ratnadeep Debnath <rtnpro@gmail.com>
2022-03-04 17:20:44 +0000

    Integrate posthog

--- vmb/matrimony/apps.py
+++ vmb/matrimony/apps.py
@@ -1,9 +1,17 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
+import posthog
+
 from .tasks import send_batch_matches_emails
 
 
 class MatrimonyConfig(AppConfig):
     name = "vmb.matrimony"
     verbose_name = _("Matrimony")
+
+    def ready(self):
+        from django.conf import settings
+
+        posthog.api_key = settings.POSTHOG_API_KEY
+        posthog.host = settings.POSTHOG_HOST
