6f292801f2103fd2848b2cb75718cd71484a689e
Vaibhav-22-dm <vaibhavdmohite22@gmail.com>
2022-12-19 18:35:36 +0530

    Added Test for profile-details view

--- vmb/matrimony/tests/test_views.py
+++ vmb/matrimony/tests/test_views.py
@@ -11,7 +11,19 @@ from vmb.matrimony.tests.factories import *
 pytestmark = pytest.mark.django_db
 
 
-def test_in_index_view(client):
+def Reusable_Profile_Generator():
+
+    user = UserCustomFactory()
+    profile = MatrimonyFactory(user=user, profile_id=user.username)
+    EmailAddress.objects.create(
+        user=profile.user, email=profile.user.email, primary=True, verified=True
+    )
+    tos = TermsOfService.objects.create(active=True, content="This is the new TOS.")
+    UserAgreement.objects.create(user=profile.user, terms_of_service=tos)
+    return profile
+
+
+def test_index_view(client):
 
     # Without Login
 
@@ -21,17 +33,33 @@ def test_in_index_view(client):
     assert "account/login.html" in (t.name for t in response.templates)
 
     # With Login
+    
+    profile = Reusable_Profile_Generator()
 
-    user = UserCustomFactory()
-    profile = MatrimonyFactory(user=user, profile_id=user.username)
-    EmailAddress.objects.create(
-        user=profile.user, email=profile.user.email, primary=True, verified=True
-    )
-    tos = TermsOfService.objects.create(active=True, content="This is the new TOS.")
-    UserAgreement.objects.create(user=profile.user, terms_of_service=tos)
-
-    client.login(username=user.username, password="wordpass123")
+    client.login(username=profile.user.username, password="wordpass123")
     response = client.get(reverse("matrimony:index"), follow=True)
 
     assert response.status_code == 200
     assert "matrimony/profile_edit.html" in (t.name for t in response.templates)
+
+
+def test_profile_details(client):
+
+    profile = Reusable_Profile_Generator()
+
+    # Without Login
+
+    response = client.get(reverse('matrimony:profile-details',args=[profile.profile_id]), follow=True)
+
+    assert response.status_code == 200
+    assert "account/login.html" in (t.name for t in response.templates)
+
+    # With Login
+
+    client.login(username=profile.user.username, password="wordpass123")
+
+    response = client.get(reverse('matrimony:profile-details',args=[profile.profile_id]), follow=True)
+
+    assert response.status_code == 200
+    assert "matrimony/profile_details.html" in (t.name for t in response.templates)
+
