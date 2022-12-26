bd6b355152d98389b6f81ca9bb0a160b7f512343
Vaibhav-22-dm <vaibhavdmohite22@gmail.com>
2022-12-26 17:32:21 +0530

    Added tests for notification views

--- vmb/matrimony/tests/test_views.py
+++ vmb/matrimony/tests/test_views.py
@@ -7,6 +7,9 @@ from tos.models import *
 from vmb.matrimony.models.profiles import *
 import datetime
 from vmb.matrimony.tests.factories import *
+import datetime
+from notifications.signals import notify
+import json
 
 pytestmark = pytest.mark.django_db
 
@@ -128,4 +131,95 @@ def test_profile_edit_religion_and_family_post_view(client, django_db_setup):
     assert response.redirect_chain[0][1] == 302
     assert response.status_code == 200
 
+def test_profile_edit_expectations_post_view(client, django_db_setup):
 
+    profile = Reusable_Profile_Generator()
+
+    client.login(username=profile.user.username, password="wordpass123")
+
+    response = client.post(
+        reverse('matrimony:profile-edit',args=['expectations']), 
+        data = { 
+            'age_from': ['26'], 'age_to': ['28'], 'height_from': ['160'], 'height_to': ['165'], 'religions': ['7', '1', '5'], 'mother_tongues': ['19', '38', '54', '58', '84', '105', '107', '156', '157'], 'countries_living_in': ['57'], 'ethnicities': ['57'], 'marital_status': ['UMR'], 'want_nri': [''], 'languages_can_speak': ['38', '58', '107'], 'languages_can_read_write': ['38', '58', '107'], 'education': ['0', '18', '50', '1', '75'], 'occupations': ['19', '18', '41', '15', '33', '65'], 'employed_in': ['PSU', 'PVT', 'BUS', 'DEF', 'SE'], 'annual_income_from_0': ['600000'], 'annual_income_from_1': ['INR'], 'annual_income_to_0': ['1500000'], 'annual_income_to_1': ['INR'], 'spiritual_status': ['A'], 'min_rounds_chanting': ['25'], 'spiritual_masters': ['1'], 'partner_description': ['Should be simple and educated.'], 'submit': ['Submit']
+        },
+        follow=True
+        )
+
+    assert response.redirect_chain[0][0] == "/"
+    assert response.redirect_chain[0][1] == 302
+    assert "Thank you! Your registration has been submitted." in [m.message for m in response.context['messages']]
+    assert response.status_code == 200
+
+def test_view_all_notifications_get_view(client, django_db_setup):
+
+    profile = Reusable_Profile_Generator()
+
+    client.login(username=profile.user.username, password="wordpass123")
+
+    response = client.get(reverse("matrimony:view_all_notifications"), follow=True)
+
+    assert response.status_code == 200
+    assert "matrimony/view_all_notifications.html" in (t.name for t in response.templates)
+
+def test_mark_all_as_read_view(client, django_db_setup):
+
+    profile = Reusable_Profile_Generator()
+
+    client.login(username=profile.user.username, password="wordpass123")
+
+    notify.send(
+        sender=profile.user,
+        recipient=profile.user, 
+        verb="registration", 
+        action_object=profile, 
+        target=profile, 
+        level="Success", 
+        description="Your registration has been successful!", 
+        public=True, 
+        timestamp=datetime.datetime.now(), 
+    )
+    
+    assert len(profile.user.notifications.unread()) == 1
+
+    response = client.get(reverse("matrimony:mark_all_as_read"), HTTP_REFERER='http://127.0.0.1:8000/view_all_notifications/',follow=True)
+
+    assert len(profile.user.notifications.unread()) == 0
+    assert response.status_code == 200
+    assert "matrimony/view_all_notifications.html" in (t.name for t in response.templates)
+
+def test_mark_as_read_view(client, django_db_setup):
+
+    profile = Reusable_Profile_Generator()
+
+    client.login(username=profile.user.username, password="wordpass123")
+
+    notify.send(
+        sender=profile.user,
+        recipient=profile.user, 
+        verb="registration", 
+        action_object=profile, 
+        target=profile, 
+        level="Success", 
+        description="Your registration has been successful!", 
+        public=True, 
+        timestamp=datetime.datetime.now(), 
+    )
+    notify.send(
+        sender=profile.user,
+        recipient=profile.user, 
+        verb="update", 
+        action_object=profile, 
+        target=profile, 
+        level="Success", 
+        description="Your update has been successful!", 
+        public=True, 
+        timestamp=datetime.datetime.now(), 
+    )
+
+    assert len(profile.user.notifications.unread()) == 2
+
+    response = client.get(reverse("matrimony:mark_as_read", args=[profile.user.notifications.unread()[0].id]), HTTP_REFERER='http://127.0.0.1:8000/view_all_notifications/',follow=True)
+
+    assert len(profile.user.notifications.unread()) == 1
+    assert response.status_code == 200
+    assert "matrimony/view_all_notifications.html" in (t.name for t in response.templates)
